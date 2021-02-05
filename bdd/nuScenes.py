4fd72b646a5fe36c2f19bf66a2f30ca352127d2b
Emma Baudoint <emma.baudoint@laposte.net>
2021-02-05 23:04:59 +0100

    add image to test function on nuscene dataset

--- bdd/nuScenes.py
+++ bdd/nuScenes.py
@@ -1,9 +1,8 @@
-!mkdir -p /data/sets/nuscenes  # Make the directory to store the nuScenes dataset in.
-!wget https://www.nuscenes.org/data/v1.0-mini.tgz  # Download the nuScenes mini split.
-!tar -xf v1.0-mini.tgz -C /data/sets/nuscenes  # Uncompress the nuScenes mini split.
-!pip install nuscenes-devkit &> /dev/null  # Install nuScenes.
+import data
 
-%matplotlib inline
-from nuscenes.nuscenes import NuScenes
+nusc = data(version='v1.0-mini', dataroot='/data', verbose=True)
 
-nusc = NuScenes(version='v1.0-mini', dataroot='/data/sets/nuscenes', verbose=True)
+
+nusc.list_scenes()
+
+
