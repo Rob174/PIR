952ec3cf8cb22b4778a294e733987cccaa07e7e0
VBaures <bauresvincent@outlook.com>
2021-02-12 21:03:13 +0100

    Ajout de nuScenes

--- bdd/nuScenes.py
+++ bdd/nuScenes.py
@@ -1,8 +1,9 @@
-import data
 
-nusc = data(version='v1.0-mini', dataroot='/data', verbose=True)
+from nuscenceskit.pythonsdk.nuscenes import nuscenes
+print("COucou")
+nusc = nuscenes.NuScenes(version='v1.0-mini', dataroot='data', verbose=True)
 
-
+print("COucou")
 nusc.list_scenes()
 
 
