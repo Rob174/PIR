0d7d68f37d55dcaaa44f40c779f182bf824522cc
Emma Baudoint <62210726+Emma-bau@users.noreply.github.com>
2021-02-17 14:35:57 +0100

    Missing files

--- bdd/nuScenes.py
+++ bdd/nuScenes.py
@@ -1,9 +1,23 @@
-
 from nuscenceskit.pythonsdk.nuscenes import nuscenes
-print("COucou")
+
 nusc = nuscenes.NuScenes(version='v1.0-mini', dataroot='data', verbose=True)
 
-print("COucou")
 nusc.list_scenes()
+
+print('Courgette')
+my_scene = nusc.scene[0]
+print(my_scene)
+first_sample_token = my_scene['first_sample_token']
+my_sample = nusc.get('sample', first_sample_token)
+print(my_sample)
+
+nusc.list_sample(my_sample['token'])
+
+sensor = 'CAM_FRONT'
+cam_front_data = nusc.get('sample_data', my_sample['data'][sensor])
+print(cam_front_data)
+
+
+nusc.render_sample_data(cam_front_data['token'])
 
 
