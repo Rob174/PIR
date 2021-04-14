03b2bf4548d3b90c8aabe55695f76441faaf53fe
Rob174 <robin.128.456@gmail.com>
2021-04-14 03:09:09 +0100

    Correction type paramètre image_width

--- IA/model_keras/train_dataset.py
+++ IA/model_keras/train_dataset.py
@@ -26,7 +26,7 @@ parser.add_argument('-bs', dest='batch_size', default=10, type=int,
                     help="[Optionnel] Indique le nombre d'images par batch")
 parser.add_argument('-gpu', dest='gpu_selected', default="0", type=str,
                     help="[Optionnel] Indique la gpu visible par le script tensorflow")
-parser.add_argument('-img_w', dest='image_width', default=400, type=str,
+parser.add_argument('-img_w', dest='image_width', default=400, type=int,
                     help="[Optionnel] Indique la gpu visible par le script tensorflow")
 args = parser.parse_args()
 
