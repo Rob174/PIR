c1e7bd9b47fad032c068d0cc8186aeb1511d6f4e
Rob174 <robin.128.456@gmail.com>
2021-04-16 14:42:40 +0100

    Ajout modèle avec tête inception  plutôt que couches simples

---
+++ IA/model_keras/model/model_inception_top.py
@@ -0,0 +1,87 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from IA.improved_graph.src.layers.base_layers import Input, Conv2D, BatchNormalization, Activation, SeparableConv2D, \
+    MaxPooling2D, Add, GlobalAveragePooling2D, Dropout, Dense, Flatten, Concatenate
+from IA.improved_graph.src.layers.node_model import *
+from tensorflow.keras.initializers import he_normal
+import tensorflow.keras.regularizers as regularizers
+
+'''cette fonction permet de créer le modéle(architecture de l'IA)'''
+'''couche par ordre input, couche de convolution(filtre, noyau,stride(pas de déplacement), padding(),batchNormalisation(ameliorer la convergence )'''
+import tensorflow as tf
+
+def inception(conv1_filters,conv3_filters,conv5_filters,pool_red_filters,input,index):
+    conv1 = Conv2D(filters=conv1_filters, kernel_size=1, strides=1, kernel_initializer=he_normal(seed=1),
+                     bias_initializer="zeros", padding='SAME', activation='relu', name='mod%d_conv1' % index)(input)
+    conv3red = Conv2D(filters=conv3_filters[0], kernel_size=1, strides=1, kernel_initializer=he_normal(seed=1),
+                        bias_initializer="zeros", padding='SAME', activation='relu', name='mod%d_conv3red' % index)(
+        input)
+    conv3 = Conv2D(filters=conv3_filters[1], kernel_size=3, strides=1, kernel_initializer=he_normal(seed=1),
+                     bias_initializer="zeros", padding='SAME', activation='relu', name='mod%d_conv3' % index)(conv3red)
+    conv5red = Conv2D(filters=conv5_filters[0], kernel_size=1, strides=1, kernel_initializer=he_normal(seed=1),
+                        bias_initializer="zeros", padding='SAME', activation='relu', name='mod%d_conv5red' % index)(
+        input)
+    conv5 = Conv2D(filters=conv5_filters[1], kernel_size=5, strides=1, kernel_initializer=he_normal(seed=1),
+                     bias_initializer="zeros", padding='SAME', activation='relu', name='mod%d_conv5' % index)(conv5red)
+    pool = MaxPooling2D(pool_size=3, strides=1, padding='SAME', name='mod%d_pool' % index)(input)
+    poolred = Conv2D(filters=pool_red_filters, kernel_size=1, strides=1, padding='SAME', activation='relu',
+                       name='mod%d_poolred' % index)(pool)
+    # Rassemble les résultats
+    conv = Concatenate(name='mod%d_concat' % index)([conv1, conv3, conv5, poolred])
+    return conv
+def make_model(input_shape, num_classes, last_activation="linear", nb_modules=4, reduction_layer="globalavgpool",
+               dropout_rate=0.5):
+    inputs = Input(shape=input_shape)
+
+    # Entry block
+    # preprocess the data
+    x = inception(conv1_filters=64,
+                    conv3_filters=[48,96],
+                    conv5_filters=[16,32],
+                    pool_red_filters=32,input=inputs,index=1)
+    x = inception(conv1_filters=96,
+                        conv3_filters=[64,128],
+                        conv5_filters=[24,48],
+                        pool_red_filters=48,input=x,index=2)
+
+    previous_block_activation = x  # Set aside residual
+    liste_filtres = [128, 256, 512, 728, 1024]
+    liste_filtres = liste_filtres[:nb_modules + 1]
+    for size in liste_filtres[:-1]:
+        x = Activation(activation="relu")(x)
+        # réduit le nombre de parametre (matrice multipliée au lieu de matrice normale)
+        x = SeparableConv2D(filters=size, kernel_size=3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        x = Activation(activation="relu")(x)
+        x = SeparableConv2D(filters=size, kernel_size=3, padding="same")(x)
+        x = BatchNormalization()(x)
+
+        # couche de pooling
+        x = MaxPooling2D(pool_size=3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = Conv2D(filters=size, kernel_size=1, strides=2, padding="same")(previous_block_activation)
+        x = Add()([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = SeparableConv2D(filters=liste_filtres[-1], kernel_size=3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation(activation="relu")(x)
+
+    if reduction_layer == "globalavgpool":
+        '''moyenne de toute les valeurs au lieu du max'''
+        x = GlobalAveragePooling2D()(x)
+    elif reduction_layer == "flatten":
+        x = Flatten()(x)
+    else:
+        raise Exception("reduction layer option not supported %s" % reduction_layer)
+
+    '''on met 50% des pixels en blanc pour eviter que le réseau se base sur les memes pixels (couche de régularisation)'''
+    x = Dropout(rate=dropout_rate)(x)
+    # couche fully connected layer
+    outputs = Dense(units=num_classes, activation=last_activation)(x)
+    return Model([inputs], [outputs], name="keras_model")
--- IA/model_keras/train_dataset.py
+++ IA/model_keras/train_dataset.py
@@ -1,13 +1,15 @@
 import argparse
 import os
 import sys
+
 chemin_fichier = os.path.realpath(__file__).split("/")
 sys.path.append("/".join(chemin_fichier[:-2]))
 sys.path.append("/".join(chemin_fichier[:-3]))
-print("/".join(chemin_fichier[:-2]+["improved_graph"]))
-sys.path.append("/".join(chemin_fichier[:-2]+["improved_graph","src","layers"]))
+print("/".join(chemin_fichier[:-2] + ["improved_graph"]))
+sys.path.append("/".join(chemin_fichier[:-2] + ["improved_graph", "src", "layers"]))
 from IA.model_keras.data.generate_data import Nuscene_dataset
-from model.model import make_model
+from IA.model_keras.model.model_orig import make_model
+from IA.model_keras.model.model_inception_top import make_model as make_model_inception_top
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -50,7 +52,6 @@ parser.add_argument('-dptRate', dest='dropout_rate', default="0.5", type=str,
                     help="[Optionnel] Indique la gpu visible par le script tensorflow")
 args = parser.parse_args()
 
-
 dataset = Nuscene_dataset(img_width=args.image_width)
 dataset.batch_size = args.batch_size
 liste_lossTr = []
@@ -66,7 +67,7 @@ accur_step = 5
 def approx_accuracy(modeApprox="none"):
     fct_approx = None
     if modeApprox == "none":
-        fct_approx = lambda x:x
+        fct_approx = lambda x: x
     elif args.approximationAccuracy == "round":
         fct_approx = tf.math.round
     elif args.approximationAccuracy == "int":
@@ -74,34 +75,40 @@ def approx_accuracy(modeApprox="none"):
     else:
         raise Exception("Unknown approximation function %s" % modeApprox)
 
-    def approx_accuracy_round(y_true,y_pred):
-        return categorical_accuracy(y_true,fct_approx(y_pred))
+    def approx_accuracy_round(y_true, y_pred):
+        return categorical_accuracy(y_true, fct_approx(y_pred))
+
     return approx_accuracy_round
 
 
-with tf.device('/GPU:'+args.gpu_selected):
-    model = make_model((dataset.image_shape[1], dataset.image_shape[0], 3,),
-                       num_classes=len(dataset.correspondances_classes.keys()),
-                       last_activation=args.lastActivation,
-                       nb_modules=args.nb_modules,reduction_layer=args.reduction_layer,
-                       dropout_rate=float(args.dropout_rate))
+with tf.device('/GPU:' + args.gpu_selected):
+    model = make_model_inception_top((dataset.image_shape[1], dataset.image_shape[0], 3,),
+                                     num_classes=len(dataset.correspondances_classes.keys()),
+                                     last_activation=args.lastActivation,
+                                     nb_modules=args.nb_modules, reduction_layer=args.reduction_layer,
+                                     dropout_rate=float(args.dropout_rate))
     model.keras_layer.compile(optimizer=Adam(learning_rate=args.lr, epsilon=args.epsilon), loss="MSE",
                               metrics=[approx_accuracy(args.approximationAccuracy)])
 from time import strftime, gmtime
 import os
+
+
 class FolderInfos:
     base_folder = None
     base_filename = None
+
     @staticmethod
     def init():
         id = strftime("%Y-%m-%d_%Hh%Mmin%Ss", gmtime())
-        FolderInfos.base_folder= "/".join(os.path.realpath(__file__).split("/")[:-3]+["data/"])+id+"/"
+        FolderInfos.base_folder = "/".join(os.path.realpath(__file__).split("/")[:-3] + ["data/"]) + id + "/"
         FolderInfos.base_filename = FolderInfos.base_folder + id
         os.mkdir(FolderInfos.base_folder)
+
+
 FolderInfos.init()
-model.save(FolderInfos.base_filename+"_bs_%d_lastAct_%s_accurApprox_%s_nbMod_%d_dpt_%s_redLay_%s_graph.dot"
-           % (dataset.batch_size,args.lastActivation,args.approximationAccuracy,
-              args.nb_modules,args.dropout_rate,args.reduction_layer))
+model.save(FolderInfos.base_filename + "_bs_%d_lastAct_%s_accurApprox_%s_nbMod_%d_dpt_%s_redLay_%s_graph.dot"
+           % (dataset.batch_size, args.lastActivation, args.approximationAccuracy,
+              args.nb_modules, args.dropout_rate, args.reduction_layer))
 iteratorValid = dataset.getNextBatchValid()
 compteur = 0
 
@@ -122,9 +129,10 @@ def plot():
     loss_axe.set_ylabel("Loss (MSE)")
     fig.legend()
     plt.grid()
-    plt.savefig(FolderInfos.base_filename+"erreur_accuracy_batch_size_%d_lastAct_%s_accurApprox_%s_nbMod_%d_dpt_%s_redLay_%s.png"
-                % (dataset.batch_size,args.lastActivation,args.approximationAccuracy,args.nb_modules,
-                   args.dropout_rate,args.reduction_layer))
+    plt.savefig(
+        FolderInfos.base_filename + "erreur_accuracy_batch_size_%d_lastAct_%s_accurApprox_%s_nbMod_%d_dpt_%s_redLay_%s.png"
+        % (dataset.batch_size, args.lastActivation, args.approximationAccuracy, args.nb_modules,
+           args.dropout_rate, args.reduction_layer))
     plt.clf()
     plt.close(fig)
 
@@ -134,7 +142,7 @@ for epochs in range(1):
     while True:
         try:
             batchImg, batchLabel = next(iteratorTr)
-            with tf.device('/GPU:'+args.gpu_selected):
+            with tf.device('/GPU:' + args.gpu_selected):
                 [loss, accuracy] = model.keras_layer.train_on_batch(batchImg, batchLabel)
             liste_lossTr.append(loss)
             liste_accuracyTr.append(accuracy)
