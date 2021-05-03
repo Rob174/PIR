e5d515233879c5235e5f6c2bd6ba31516e2c9028
nsmaniotto <smaniotto.nathan@gmail.com>
2021-05-03 18:20:14 +0200

    Fetching images in the right dataset, now counting the elements of each entire/training/validation dataset

--- IA/PilotNet/data/NuSceneDB.py
+++ IA/PilotNet/data/NuSceneDB.py
@@ -1,5 +1,5 @@
 from typing import List
-import os
+import os.path
 
 import scipy.misc
 import random
@@ -39,12 +39,12 @@ class NuSceneDB(object):
 
     """Preprocess images of the road ahead and expected outputs."""
 
-    def __init__(self, batch_size):
+    def __init__(self, batch_size, tr_prct=0.6):
         imgs: List[str] = []  # Will store the path of each image
         outputs: List[float] = []  # Will store the occurrence of each label for each image
 
         # Extract images annotations
-        data_path = "/scratch/rmoine/PIR/nuscene"
+        data_path = "/scratch/rmoine/PIR/nuscene/"
         json_path = "/scratch/rmoine/PIR/extracted_data_nusceneImage.json"
 
         with open(json_path) as json_file:
@@ -75,9 +75,23 @@ class NuSceneDB(object):
                     imgs.append(data_path + filename)
                     outputs.append(image_outputs)
 
+        self.dataset_size = len(imgs)
+        self.training_dataset_size = self.dataset_size * tr_prct
+        self.validation_dataset_size = self.dataset_size - self.training_dataset_size
+
+        print("[NuSceneDB] number of images within the entire dataset : " + str(self.dataset_size))
+        print("[NuSceneDB] number of images within the training dataset : " + str(self.training_dataset_size))
+        print("[NuSceneDB] number of images within the validation dataset : " + str(self.validation_dataset_size))
+
+        self.dataset_tr = list(range(0, int(len(imgs) * tr_prct)))
+        self.dataset_valid = list(range(int(len(imgs) * tr_prct), len(imgs)))
+
         # get number of images
         self.num_images = len(imgs)
         self.batch_size = batch_size
+
+        self.imgs_path = imgs
+        self.outputs_values = outputs
 
         self.train_imgs_path: List[str] = imgs[:int(self.num_images * 0.8)]
         self.train_outputs_values: List[float] = outputs[:int(self.num_images * 0.8)]
@@ -91,19 +105,18 @@ class NuSceneDB(object):
     def train_batch_generator(self):
         batch_imgs = []
         batch_outputs = []
-        index_elems: List[int] = list(range(self.num_train_images))
 
         # Shuffle the elements, will be retrieved by their index
-        random.shuffle(index_elems)
+        random.shuffle(self.dataset_tr)
 
-        for i, index_elem in enumerate(index_elems):
-            img_path: str = self.train_imgs_path[index_elem]
-            outputs_value: float = self.train_outputs_values[index_elem]
+        for index_elem in self.dataset_tr:
+            img_path: str = self.imgs_path[index_elem]
+            outputs_value: float = self.outputs_values[index_elem]
             img = Image.open(img_path).resize(size=(200, 66))
             img = np.array(img) / 255.0
             batch_imgs.append(img)
             batch_outputs.append(outputs_value)
-            if len(batch_imgs) % self.batch_size == 0 and i > 0:
+            if len(batch_imgs) % self.batch_size == 0 and len(batch_imgs) > 0:
                 batches = np.stack(batch_imgs, axis=0), np.stack(batch_outputs, axis=0)
                 batch_imgs, batch_outputs = [], []
                 yield batches
@@ -111,19 +124,18 @@ class NuSceneDB(object):
     def val_batch_generator(self):
         batch_imgs = []
         batch_outputs = []
-        index_elems: List[int] = list(range(self.num_val_images))
 
         # Shuffle the elements, will be retrieved by their index
-        random.shuffle(index_elems)
+        random.shuffle(self.dataset_valid)
 
-        for i, index_elem in enumerate(index_elems):
-            img_path: str = self.val_imgs_path[index_elem]
-            outputs_value: float = self.val_outputs_values[index_elem]
+        for index_elem in self.dataset_valid:
+            img_path: str = self.imgs_path[index_elem]
+            outputs_value: float = self.outputs_values[index_elem]
             img = Image.open(img_path).resize(size=(200, 66))
             img = np.array(img)
             batch_imgs.append(img / 255.0)
             batch_outputs.append(outputs_value)
-            if len(batch_imgs) % self.batch_size == 0 and i > 0:
+            if len(batch_imgs) % self.batch_size == 0 and len(batch_imgs) > 0:
                 batches = np.stack(batch_imgs, axis=0), np.stack(batch_outputs, axis=0)
                 batch_imgs, batch_outputs = [], []
                 yield batches
