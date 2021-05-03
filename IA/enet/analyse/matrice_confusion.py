aa78c2e4c9dc1346e55ac71a806e524685708e8e
Rob174 <robin.128.456@gmail.com>
2021-05-03 14:48:17 +0100

    Séparation des fonctionnalités de la matrice de confusion pour favoriser la réutilisation du code

--- IA/enet/analyse/matrice_confusion.py
+++ IA/enet/analyse/matrice_confusion.py
@@ -1,4 +1,5 @@
 from IA.model_keras.analyse.matrices_confusion import MakeConfusionMatrix
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import cv2
 
@@ -8,19 +9,33 @@ class MakeConfusionMatrixEnet(MakeConfusionMatrix):
     On peut ensuite détecter les contours et compter le nombre d'objets"""
 
 
-    def __init__(self,seuil_threshold,*args,**kargs):
+    def __init__(self,*args,**kargs):
         super(MakeConfusionMatrixEnet, self).__init__(*args,**kargs)
-        self.seuil_threshold = seuil_threshold
+        self.seuils_threshold = [0.5,0.75,0.90]
     def add_sample(self,batch_img, batch_true):
         batch_pred = self.model.predict(batch_img)
         batch_true = np.array(batch_true.numpy(), dtype=np.float32)
         for i_batch in range(len(batch_pred)):
             for i_classe in range(self.nb_classes):
                 for batch, dest in zip([batch_true,batch_pred],["true","pred"]):
-                    img_threshold = cv2.threshold(batch[i_batch,:,:, i_classe],
-                                                  self.seuil_threshold,
-                                                  maxval=1.,
-                                                  type=cv2.THRESH_BINARY)
-                    img_threshold = np.array(img_threshold,dtype=np.uint8)
-                    nb_contours = len(cv2.findContours(img_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE))
-                    self.matrices_confusion[i_classe][dest].append(nb_contours)
+                    for seuil_threshold in self.seuils_threshold:
+                        img_threshold = cv2.threshold(batch[i_batch,:,:, i_classe],
+                                                      seuil_threshold,
+                                                      maxval=1.,
+                                                      type=cv2.THRESH_BINARY)
+                        img_threshold = np.array(img_threshold,dtype=np.uint8)
+                        nb_contours = len(cv2.findContours(img_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE))
+                        self.matrices_confusion[i_classe][dest][seuil_threshold].append(nb_contours)
+    def __call__(self):
+        # Constitution de la matrice de confusion finale
+        self.matrices_confusion = [{"true": {k:[] for k in self.seuils_threshold}, "pred": {k:[] for k in self.seuils_threshold}} for _ in range(self.nb_classes)]
+        for batch_img, batch_true in self.dataset:
+            self.add_sample(batch_img, batch_true)
+        # Création des matrices de confusion (1 par classe)
+        for i, classe_data in enumerate(self.matrices_confusion):
+            for seuil_threshold in self.seuils_threshold:
+                labels = list({val for val in (classe_data["true"][seuil_threshold] + classe_data["pred"][seuil_threshold])})
+                matrice_confusion_petite = confusion_matrix(y_true=classe_data["true"][seuil_threshold],
+                                                            y_pred=classe_data["pred"][seuil_threshold],
+                                                            labels=labels)
+                self.save_confusion_matrix(matrice_confusion_petite, labels,i,identifieur_nom=f"seuil-{seuil_threshold}")
--- IA/model_keras/analyse/matrices_confusion.py
+++ IA/model_keras/analyse/matrices_confusion.py
@@ -23,6 +23,66 @@ class MakeConfusionMatrix:
             for i_classe in range(self.nb_classes):
                 self.matrices_confusion[i_classe]["true"].append(batch_true[i_batch, i_classe])
                 self.matrices_confusion[i_classe]["pred"].append(batch_pred[i_batch, i_classe])
+    def save_confusion_matrix(self,matrice_confusion_petite,labels,classe_index,identifieur_nom=""):
+        total = np.sum(matrice_confusion_petite)
+        nb_val = len(labels)
+        matrice_confusion_prct_petite = matrice_confusion_petite / total * 100
+        matrice_confusion_prct = np.zeros((nb_val + 1, nb_val + 1))
+        matrice_confusion_prct[:nb_val, :nb_val] = matrice_confusion_prct_petite
+        matrice_confusion_prct[nb_val, nb_val] = np.sum(np.diag(matrice_confusion_prct_petite))
+
+        matrice_confusion = np.zeros((nb_val + 1, nb_val + 1))
+        matrice_confusion[:nb_val, :nb_val] = matrice_confusion_petite
+        matrice_confusion[nb_val, nb_val] = np.sum(np.diag(matrice_confusion_petite))
+        top3voisins_precision = np.sum(np.diag(matrice_confusion_prct_petite)) \
+                                + np.sum(np.diag(matrice_confusion_prct_petite, k=-1)) \
+                                + np.sum(np.diag(matrice_confusion_prct_petite, k=1))
+        matrice_confusion_prct = np.round(matrice_confusion_prct, decimals=2)
+        matrice_confusion_prct_petite = np.round(matrice_confusion_prct_petite, decimals=2)
+        top3voisins_precision = np.round(top3voisins_precision, decimals=2)
+        fig = plt.figure(figsize=(20, 20))
+        plt.imshow(matrice_confusion_prct)
+        plt.title(
+            f"Matrice de confusion de la classe {classe_index} {identifieur_nom}: {self.correspondances_index_classes[classe_index]} \nPrécision : {matrice_confusion_prct[-1, -1]:.2f} % ; Top-3 précision : {top3voisins_precision:.2f} %")
+        ax = plt.gca()
+        ax.xaxis.set_label_position('top')
+        ax.xaxis.tick_top()
+        ax.set_xlabel("Valeurs prédites")
+        ax.set_ylabel("Valeurs vraies")
+        ax.set_xticks(ticks=list(range(len(labels))))
+        ax.set_yticks(ticks=list(range(len(labels))))
+        ax.set_xticklabels(labels)
+        ax.set_yticklabels(labels)
+        for x, y in np.ndindex(nb_val + 1, nb_val + 1):
+            if (x < nb_val and y < nb_val) or x == y:
+                plt.text(y, x, "    %.2f%%    \n    %d     " % (matrice_confusion_prct[x, y], matrice_confusion[x, y]),
+                         ha="center", va="center", color="red")
+        plt.tight_layout()
+        plt.colorbar()
+        plt.set_cmap('Blues')
+        # get image in numpy array (thanks to https://stackoverflow.com/questions/7821518/matplotlib-save-plot-to-numpy-array)
+        fig.canvas.draw()
+        data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        data = np.stack((data,), axis=0)
+        if self.summary_writer is not None:
+            with self.summary_writer.as_default():
+                tf.summary.image(f"matrice_confusion_{self.correspondances_index_classes[classe_index]}_{identifieur_nom}", data, step=0)
+                self.summary_writer.flush()
+        array_matrice = np.array([['' for _ in range(len(labels) + 2)] for _ in range(len(labels) + 3)], dtype="U25")
+        array_matrice[0, :] = np.array(['Valeurs predites'] + [str(i) for i in labels] + [''], dtype=np.str)
+        array_matrice[1, 0] = "Valeurs reelles"
+        array_matrice[2:-1, 0] = np.array([str(i) for i in labels], dtype=np.str)
+        array_matrice[2:-1, 1:-1] = addStr(
+            addStr(
+                addStr(np.array(matrice_confusion_petite, dtype=np.str), " | "),
+                np.array(np.round(matrice_confusion_prct_petite, decimals=2), dtype=np.str)
+            ), "%"
+        )
+        array_matrice[-1, -1] = str(matrice_confusion[-1, -1]) + "\n" + str(
+            np.round(matrice_confusion_prct[-1, -1], decimals=2)) + "%"
+        df = pd.DataFrame(array_matrice)
+        df.to_csv(FolderInfos.base_filename + f"matrice_confusion_{self.correspondances_index_classes[classe_index]}_{identifieur_nom}.csv")
     def __call__(self):
         # Constitution de la matrice de confusion finale
         self.matrices_confusion = [{"true":[],"pred":[]} for _ in range(self.nb_classes)]
@@ -34,63 +94,8 @@ class MakeConfusionMatrix:
             matrice_confusion_petite = confusion_matrix(y_true=classe_data["true"],
                                                  y_pred=classe_data["pred"],
                                                  labels=labels)
-            total = np.sum(matrice_confusion_petite)
-            nb_val = len(labels)
-            matrice_confusion_prct_petite = matrice_confusion_petite / total * 100
-            matrice_confusion_prct = np.zeros((nb_val+1,nb_val+1))
-            matrice_confusion_prct[:nb_val,:nb_val] = matrice_confusion_prct_petite
-            matrice_confusion_prct[nb_val,nb_val] = np.sum(np.diag(matrice_confusion_prct_petite))
+            self.save_confusion_matrix(matrice_confusion_petite,labels,i)
 
-            matrice_confusion = np.zeros((nb_val+1,nb_val+1))
-            matrice_confusion[:nb_val,:nb_val] = matrice_confusion_petite
-            matrice_confusion[nb_val,nb_val] = np.sum(np.diag(matrice_confusion_petite))
-            top3voisins_precision = np.sum(np.diag(matrice_confusion_prct_petite)) \
-                                    + np.sum(np.diag(matrice_confusion_prct_petite,k=-1)) \
-                                    + np.sum(np.diag(matrice_confusion_prct_petite,k=1))
-            matrice_confusion_prct = np.round(matrice_confusion_prct,decimals=2)
-            matrice_confusion_prct_petite = np.round(matrice_confusion_prct_petite, decimals=2)
-            top3voisins_precision = np.round(top3voisins_precision,decimals=2)
-            fig = plt.figure(figsize=(20,20))
-            plt.imshow(matrice_confusion_prct)
-            plt.title(f"Matrice de confusion de la classe {i} : {self.correspondances_index_classes[i]} \nPrécision : {matrice_confusion_prct[-1,-1]:.2f} % ; Top-3 précision : {top3voisins_precision:.2f} %")
-            ax = plt.gca()
-            ax.xaxis.set_label_position('top')
-            ax.xaxis.tick_top()
-            ax.set_xlabel("Valeurs prédites")
-            ax.set_ylabel("Valeurs vraies")
-            ax.set_xticks(ticks=list(range(len(labels))))
-            ax.set_yticks(ticks=list(range(len(labels))))
-            ax.set_xticklabels(labels)
-            ax.set_yticklabels(labels)
-            for x, y in np.ndindex(nb_val+1, nb_val+1):
-                if (x < nb_val and y < nb_val) or x==y:
-                    plt.text(y,x, "    %.2f%%    \n    %d     " % (matrice_confusion_prct[x, y],matrice_confusion[x, y]),
-                       ha="center", va="center", color="red")
-            plt.tight_layout()
-            plt.colorbar()
-            plt.set_cmap('Blues')
-            # get image in numpy array (thanks to https://stackoverflow.com/questions/7821518/matplotlib-save-plot-to-numpy-array)
-            fig.canvas.draw()
-            data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-            data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-            data = np.stack((data,), axis=0)
-            if self.summary_writer is not None:
-                with self.summary_writer.as_default():
-                    tf.summary.image(f"matrice_confusion_{self.correspondances_index_classes[i]}", data, step=0)
-                    self.summary_writer.flush()
-            array_matrice = np.array([['' for _ in range(len(labels)+2)] for _ in range(len(labels)+3)],dtype="U25")
-            array_matrice[0,:] = np.array(['Valeurs predites']+ [str(i) for i in labels]+[''],dtype=np.str)
-            array_matrice[1,0] = "Valeurs reelles"
-            array_matrice[2:-1,0] = np.array([str(i) for i in labels],dtype=np.str)
-            array_matrice[2:-1,1:-1] = addStr(
-                addStr(
-                    addStr(np.array(matrice_confusion_petite,dtype=np.str)," | "),
-                np.array(np.round(matrice_confusion_prct_petite,decimals=2),dtype=np.str)
-                ),"%"
-            )
-            array_matrice[-1,-1] = str(matrice_confusion[-1,-1]) +"\n"+ str(np.round(matrice_confusion_prct[-1,-1],decimals=2))+"%"
-            df = pd.DataFrame(array_matrice)
-            df.to_csv(FolderInfos.base_filename+f"matrice_confusion_{self.correspondances_index_classes[i]}.csv")
 
 if __name__ == "__main__":
     MakeConfusionMatrix(None,[(np.random.randint(0,1,(10,255,255,3)),np.random.choice([i for i in range(100) if i<10 or i > 90],(10,1,23)))
